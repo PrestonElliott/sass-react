8845ebac907b8224f1225980dc04a20e04632a21
Preston Elliott <preston.elliott24@gmail.com>
2021-01-13 21:26:42 -0500

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="top-container">
+        <h1 className="title">Stoplight!</h1>
+        <button className="green">Green</button>
+        <button className="yellow">Yellow</button>
+        <button className="red">Red</button>
+      </div>
     </div>
   );
 }
